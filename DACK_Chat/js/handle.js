b119ef19a4a624b4a9bb0de235a4382b0213a0cf
lenguyenhoaiphu <lenguyenhoaiphu@gmail.com>
2016-06-06 14:49:05 +0700

    CommitAuthenticationFacebook

--- DACK_Chat/js/handle.js
+++ DACK_Chat/js/handle.js
@@ -1,9 +1,17 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
+    var ref = new Firebase("https://01chatgroupp2q.firebaseio.com");
+    $scope.Logout=function () {
+        ref.unauth();
+        console.log("Logout success!!!");
+        window.location.href = "login/loginhtml.html";
+
+    }
    $scope.newroom = function(){
    	$scope.showform = !$scope.showform;
-   } 
+   }
    $scope.saveroom = function(){
    	$scope.showform = !$scope.showform;
    }
+
 });
